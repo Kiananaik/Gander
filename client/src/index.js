59ffc81f1bbd38e09fcee0b899a9a3c4eb5d9049
Queen Zcosha <kngarc@gmail.com>
2020-06-12 17:18:29 -0500

    using cors to allow igdb request

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+
+
+var cors = require('cors');
+app.use(cors());
+
 ReactDOM.render(<App />, document.getElementById("root"));
