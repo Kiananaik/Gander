cacea2721553db325058133e8b8a8edda8735c1c
Queen Zcosha <kngarc@gmail.com>
2020-06-16 20:10:29 -0500

    coding substance coming soon

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import Directory from "./components/Directory/Directory";
-// import NavBar from "./components/NavBar/NavBar";
 import logo from "./ganderLogo.svg";
 import "./App.css";
 
 function App() {
+  
   return (
     <div className="App">
+
       <div className="App-header">
+
         {/* <NavBar></NavBar> */}
         <div className="header">
-        <img src={logo} className="App-logo" alt="logo" />Let's Take a Gander at some Games...
+        Let's Take a <img src={logo} className="App-logo" alt="logo" /> at some Games...
         </div>
       </div>
       <Directory></Directory>
